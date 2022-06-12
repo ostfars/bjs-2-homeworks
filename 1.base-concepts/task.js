66add614c4f6d613606a98f285a3f36865fc4164
Vladimir Minin <v_minin@rambler.ru>
2022-06-12 14:50:22 +0300

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,23 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant = b**2 - 4*a*c;
+  switch(discriminant) {
+    case discriminant === 0:
+      arr.push(-b/(2*a))
+    break;
+    case discriminant > 0:
+      arr.push((-b + Math.sqrt(d))/(2*a))
+      arr.push((-b - Math.sqrt(d))/(2*a))
+    break;
+    default:
+
+    break;
+  }
   return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
