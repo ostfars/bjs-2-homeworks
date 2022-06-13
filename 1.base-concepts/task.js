b7a93cac85f70ebc2f088a29bdcd04278eee3c36
ostfars <v_minin@rambler.ru>
2022-06-13 15:04:35 +0300

    Решение первой задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,19 +2,16 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = b**2 - 4*a*c;
-  switch(discriminant) {
-    case discriminant === 0:
-      arr.push(-b/(2*a))
-    break;
-    case discriminant > 0:
-      arr.push((-b + Math.sqrt(d))/(2*a))
-      arr.push((-b - Math.sqrt(d))/(2*a))
-    break;
-    default:
-
-    break;
+  let d = b**2 - 4*a*c,
+      x1 = (-b + Math.sqrt(d))/(2*a),
+      x2 = (-b - Math.sqrt(d))/(2*a);
+ 
+  if (d === 0) {
+   arr.push(x1);  
+  } else if (d > 0) {
+    arr.push(x1, x2)
   }
+  
   return arr; // array
 }
 
