e51a5f6efde949fb852ba67e4dca45a056eb33dc
ostfars <v_minin@rambler.ru>
2022-06-15 23:20:29 +0300

    три задания на функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,59 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0], max = arr[0], sum = 0, avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i]
+    } else if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i])
+    if (sum > max) {
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0], max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+      max = arr[i]
+    } else if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  
+  let diff = max - min;
+  
+  return diff;
 }
+
