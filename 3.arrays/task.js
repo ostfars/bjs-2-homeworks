7d28ac8db480c2f8b13f03ad4ed1ac371f11264d
ostfars <v_minin@rambler.ru>
2022-06-19 15:48:12 +0300

    task 1 solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,22 @@
+const arr = require("jshint/data/non-ascii-identifier-start");
+
 function compareArrays(arr1, arr2) {
   let result;
+
+  result = arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])
+
+  // if (arr1.length === arr2.length) {
+  //   result = arr1.every((item, i) => item === arr2[i]);
+  // } else {
+  //   result = false;
+  // }
 
   // Ваш код
 
   return result; // boolean
 }
+
+// const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])
 
 function advancedFilter(arr) {
   let resultArr;
