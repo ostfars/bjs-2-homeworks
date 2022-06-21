8d3bb79ad34d76e9e02e0b8363fa3a2c31a38705
Vladimir <v_minin@rambler.ru>
2022-06-21 12:01:58 +0300

    gitignore added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,18 @@
 function Student(name, gender, age) {
     // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const student1 = new Student(Ahmad, male, 21);
+const student2 = new Student(Mahmud, male, 19);
+const student3 = new Student(Zohra, female, 22);
+const student4 = new Student(Fatemeh, female, 18);
+const student5 = new Student(Ali, male, 20);
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
-}
+};
 
 // ваш код для остальных методов
