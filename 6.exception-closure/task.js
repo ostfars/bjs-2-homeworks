5cf0b6cb718e4fcb05e04ad49ee7cd17353d76a0
ostfars <v_minin@rambler.ru>
2022-07-03 11:37:24 +0300

    task 2 solved

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,3 +1,5 @@
+// Task 1
+
 const parseCount = (num) => {
   if (isNaN(Number.parseInt(num))) {
     throw new Error('Невалидное значение')
@@ -13,4 +15,39 @@ const validateCount = (num) => {
     return err
   } 
   return parseCount(num)
+}
+
+// Task 2
+
+class Triangle {
+  constructor(a, b, c) {
+    if ((a + b) < c || (b + c) < a || (a + c) < b) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.sideA = a;
+    this.sideB = b;
+    this.sideC = c;
+  }
+  
+  getPerimeter() {
+    const perimeter = this.sideA + this.sideB + this.sideC
+    return perimeter
+  }
+  
+  getArea() {
+    const p = this.getPerimeter() / 2
+    const triangleArea = (Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))).toFixed(3)
+    return +triangleArea
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c)
+  } catch(err) {
+    return {
+      getArea: () => 'Ошибка! Треугольник не существует',
+      getPerimeter: () => 'Ошибка! Треугольник не существует'
+    }
+  }
 }
