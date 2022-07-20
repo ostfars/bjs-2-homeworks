33184e07e23b97083942801899a3926d6b079e3a
ostfars <v_minin@rambler.ru>
2022-07-20 14:24:27 +0300

    decorators task 1 solved

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,11 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.find((item) => item.hash === hash);
+      if (objectInCache) { 
+          console.log("Из кэша: " + objectInCache.value)
+          return "Из кэша: " + objectInCache.value;
+      }
+
+      let result = func(...args); 
+      cache.push({'hash': `${hash}`, 'value': `${result}`}); 
+      if (cache.length > 5) { 
+        cache.shift()
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timerId;
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      f.apply(this, args);
+      console.timeEnd('time'); // согласуется с console.time() и используется для засекания времени
+    }, ms)
+  }
 }
+console.time('time')
+const sendSignal = () => console.log("Сигнал отправлен");
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+
+
 
 function debounceDecorator2(func) {
   // Ваш код
