e1eb3952470107763ffffb6b15db75dc5c0c7f7a
ostfars <v_minin@rambler.ru>
2022-07-21 11:51:47 +0300

    decorators task 2 solved

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -21,23 +21,46 @@ function cachingDecoratorNew(func) {
 }
 
 
-function debounceDecoratorNew(func) {
+const debounceDecoratorNew = (f, ms) => {
+ 
   let timerId;
+   
   return function (...args) {
+    if (!timerId) {
+      f.apply(this, args)
+    }
+    
     clearTimeout(timerId);
-
-    timerId = setTimeout(() => {
+      timerId = setTimeout(() => {
       f.apply(this, args);
-      console.timeEnd('time'); // согласуется с console.time() и используется для засекания времени
     }, ms)
   }
 }
-console.time('time')
-const sendSignal = () => console.log("Сигнал отправлен");
-const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+
+// const sendSignal = () => console.log("Сигнал отправлен");
+// const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+// setTimeout(upgradedSendSignal); // Сигнал отправлен
+// setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
+// setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
+// setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
+// setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
+// setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
+// setTimeout(upgradedSendSignal, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
 
 
 
 function debounceDecorator2(func) {
-  // Ваш код
+  f()
+  
+  let timerId;
+   
+  return function (...args) {
+
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      f.apply(this, args);
+    }, ms)
+  }
+  
 }
