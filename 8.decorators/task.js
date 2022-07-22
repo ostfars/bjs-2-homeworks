b22b01f029f6a0f3c0b3706d4b93f9fa070e80c4
ostfars <v_minin@rambler.ru>
2022-07-22 10:46:53 +0300

    all tasks solved

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -22,7 +22,6 @@ function cachingDecoratorNew(func) {
 
 
 const debounceDecoratorNew = (f, ms) => {
- 
   let timerId;
    
   return function (...args) {
@@ -37,30 +36,20 @@ const debounceDecoratorNew = (f, ms) => {
   }
 }
 
-// const sendSignal = () => console.log("Сигнал отправлен");
-// const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
-// setTimeout(upgradedSendSignal); // Сигнал отправлен
-// setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
-// setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
-// setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
-// setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
-// setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
-// setTimeout(upgradedSendSignal, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
-
-
-
 function debounceDecorator2(func) {
-  f()
-  
+  let count = 0;
   let timerId;
    
   return function (...args) {
-
+    if (!timerId) {
+      f.apply(this, args)
+    }
+    
     clearTimeout(timerId);
-
-    timerId = setTimeout(() => {
+      timerId = setTimeout(() => {
       f.apply(this, args);
     }, ms)
+    count++;
+    // console.log(count)
   }
-  
 }
